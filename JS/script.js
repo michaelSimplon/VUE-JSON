9a37762cec6d6709b17ee771bb1766d7679ea4d2
micki <petitzeppelin@gmail.com>
2019-03-11 08:08:42 +0100

    update composant

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,22 @@
-Vue.component({})
+Vue.component('list-adherents',{
+    props:['data', 'titres'],
+    template: `
+    <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col" v-for="titre in titres">{{titre.toUpperCase()}}</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr v-for="adherent in data">
+                <td>{{adherent.name}}</td>
+                <td>{{adherent.company}}</td>
+                <td>{{adherent.age}} ans</td>
+            </tr>
+        </tbody>
+    </table>
+    `
+})
 
 var monVue = new Vue({
     el:"#content",
